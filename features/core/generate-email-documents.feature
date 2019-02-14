ae8de1598212c7dab15d7db2123fce691b1e9465
Joel Dunham <jrwdunham@gmail.com>
2019-02-13 19:40:55 -0800

    Test generation of AR OP DEMAND v. 10
    - Make doc-generation validator specification more expressive.
    - Set DGS requirement to DG-3-grouping-permit-templates-update dev
      branch.

--- features/core/generate-email-documents.feature
+++ features/core/generate-email-documents.feature
@@ -7,13 +7,22 @@
 #           -D ess_url=http://127.0.0.1:61780/micros/ess/v1/api/ \
 #           -D ess_access_token=<ESS_TOKEN> \
 #           --no-skipped
+#
+# To target a specific template, e.g., the
+# ar_op_friendly_reminder_consolidated_email template row of the "templates and
+# contexts" table), add the following tag::
+#
+#     $ behave \
+#           --tags=generate-ar-op-cons-emails \
+#           --tags=template.template_key.ar_op_friendly_reminder_consolidated_email \
+#           ...
 
 @generate-ar-op-cons-emails
 Feature: AR OP Consolidated HTML Email Generation
   Clients of the TSBC NC APIs want to be able to generate HTML email documents
   using the Email Sending Service (ESS).
 
-  @production
+  @template.template_key.<template_key> @production
   Scenario Outline: Dan wants to generate the Accounts Receivable (AR) Operating Permit (OP) renewal HTML email documents using the DGS and confirm that the generated documents have the expected properties.
     Given a DGS instance containing an up-to-date template <template_key>, including its template dependencies
     When a document of type <output_type> is generated from template <template_key> using data context <context_path>
@@ -23,4 +32,6 @@ Feature: AR OP Consolidated HTML Email Generation
     Examples: templates and contexts
     | template_key                               | output_type | context_path                              |
     | ar_op_friendly_reminder_consolidated_email | text/html   | ar-op-friendly-reminder-consolidated.json |
+    | ar_op_past_due_consolidated_email          | text/html   | ar-op-past-due-consolidated.json          |
+    | ar_op_demand_consolidated_email            | text/html   | ar-op-demand-consolidated.json            |
     | ar_op_final_notice_consolidated_email      | text/html   | ar-op-final-notice-consolidated.json      |
--- features/steps/generate_email_documents_steps.py
+++ features/steps/generate_email_documents_steps.py
@@ -1,5 +1,6 @@
 """Steps for the "Generate Email Documents" feature."""
 
+from collections import namedtuple
 import logging
 import os
 import pprint
@@ -59,10 +60,10 @@ def step_impl(context):
 
 @then('the generated document is rendered correctly')
 def step_impl(context):
-    recipe = (
-        context.scenario.generated_document_params['output_type'],
-        context.scenario.generated_document_params['template_key'],
-        context.scenario.generated_document_params['context_path'],)
+    recipe = Recipe(
+        template=context.scenario.generated_document_params['template_key'],
+        context=context.scenario.generated_document_params['context_path'],
+        otype=context.scenario.generated_document_params['output_type'],)
     validate_document_generation(
         recipe,
         context.scenario.downloaded_doc_path)
@@ -71,29 +72,7 @@ def step_impl(context):
 # Utils
 # ------------------------------------------------------------------------------
 
-AR_OP_FRIENDLY_REMINDER_CONSOLIDATED_EMAIL_RECIPE = (
-    'text/html',
-    'ar_op_friendly_reminder_consolidated_email',
-    'ar-op-friendly-reminder-consolidated.json')
-
-AR_OP_FINAL_NOTICE_CONSOLIDATED_EMAIL_RECIPE = (
-    'text/html',
-    'ar_op_final_notice_consolidated_email',
-    'ar-op-final-notice-consolidated.json')
-
-
-def get_total_amount_due_tds(total_amount_due):
-    return (
-        f'<td colspan=5 style="border: 1px solid #C1C7D0; margin: 0px; padding:'
-        f' 0.5em;">Total amount due for all invoices</td> <td style="border:'
-        f' 1px solid #C1C7D0; margin: 0px; padding: 0.5em; text-align:'
-        f' right;">{total_amount_due}</td>')
-
-
-def get_notice_title_div(title_text):
-    return (
-        f'<div style="text-align: center"> <span style="font-size:'
-        f' 16px;"><strong> {title_text} </strong></span> <br> </div>')
+Recipe = namedtuple('Recipe', 'template, context, otype')
 
 
 FEE_DESCRIPTION_TH = (
@@ -105,33 +84,129 @@ ACCOUNT_NUMBER_RIGHT_ALIGNED_TD = (
     ' right; padding: 0.25em 0.25em;"><strong>Account Number:</strong></td>')
 
 
-# Map 3-tuple recipes for document generation (output_type, template_key,
-# context_path) to tuples of "needles" (i.e., substrings) that should be
-# found in the haystack that is the generated document.
-NEEDLES_BY_RECIPE = {
+def confirm_total_amount_due_is(total_amount_due):
+    def f(document_str):
+        if (f'<td colspan=5 style="border: 1px solid #C1C7D0; margin: 0px;'
+            f' padding: 0.5em;">Total amount due for all invoices</td> <td'
+            f' style="border: 1px solid #C1C7D0; margin: 0px; padding: 0.5em;'
+            f' text-align: right;">{total_amount_due}</td>'
+            ) not in document_str:
+            return (f'Failed to find total amount due {total_amount_due} in the'
+                    ' document.')
+    return f
 
-    AR_OP_FRIENDLY_REMINDER_CONSOLIDATED_EMAIL_RECIPE:
+
+def confirm_notice_title_is(title_text):
+    def f(document_str):
+        if  (f'<div style="text-align: center"> <span style="font-size:'
+             f' 16px;"><strong> {title_text} </strong></span> <br> </div>'
+             ) not in document_str:
+            return (f'Failed to find expected text "{title_text}" in the'
+                    f' document.')
+    return f
+
+
+def fee_descr_col_is_present():
+    def f(document_str):
+        if FEE_DESCRIPTION_TH not in document_str:
+            return f'Failed to locate a "fee description" table column'
+    return f
+
+
+def account_number_invisi_table_is_twice_present():
+    def f(document_str):
+        prsnc_cnt = document_str.count(ACCOUNT_NUMBER_RIGHT_ALIGNED_TD)
+        if prsnc_cnt != 2:
+            return (f'The right-aligned borderless "account number" table was'
+                    f' found {prsnc_cnt} times. It should have been present 2'
+                    f' times.')
+    return f
+
+
+def collection_action_stmnt_removed():
+    def f(document_str):
+        if ('Failure to forward payment by the expiry date may result in'
+            ' collection action') in document_str:
+            return (f'The collection action statement was not removed. It'
+                    f' should have been removed.')
+    return f
+
+
+def ssgr_s18_italicized_present():
+    def f(document_str):
+        if (f'<i>Safety Standards General Regulations (SSGR) s. 18</i>'
+                ) not in document_str:
+            return 'The italicized SSGR s. 18 was not cited.'
+    return f
+
+
+def collection_mention_removed():
+    def f(document_str):
+        if (('to avoid collection and enforcement actions' in document_str) or
+                ('to avoid enforcement actions' not in document_str)):
+            return 'The "collection actions" mention was not removed.'
+    return f
+
+
+def invoice_changed_to_notice():
+    def f(document_str):
+        if ('If you have questions about this notice or your permit(s)'
+                ) not in document_str:
+            return 'The "this invoice" should have been changed to "this notice".'
+    return f
+
+
+# Map 3-tuple recipes for document generation (template_key, context_path,
+# output_type) to tuples of "validators", functions that expect the generated
+# document as their sole argument.
+VALIDATORS_BY_RECIPE = {
+
+    Recipe(  # for Friendly Reminder
+        template='ar_op_friendly_reminder_consolidated_email',
+        context='ar-op-friendly-reminder-consolidated.json',
+        otype='text/html',):
     (
-     # Amounts due get summed correctly
-     get_total_amount_due_tds('$3,301.74'),
-     # Correct notice title <div>
-     get_notice_title_div('FRIENDLY REMINDER OPERATING PERMIT RENEWAL NOTICE'),
-     # Fee description column is present in invoices table
-     FEE_DESCRIPTION_TH,
-     # Account number right-aligned table cell is present
-     ACCOUNT_NUMBER_RIGHT_ALIGNED_TD,
+        confirm_total_amount_due_is('$3,301.74'),
+        confirm_notice_title_is('FRIENDLY REMINDER OPERATING PERMIT RENEWAL NOTICE'),
+        fee_descr_col_is_present(),
+        account_number_invisi_table_is_twice_present(),
     ),
 
-    AR_OP_FINAL_NOTICE_CONSOLIDATED_EMAIL_RECIPE:
+    Recipe(  # for Past Due
+        template='ar_op_past_due_consolidated_email',
+        context='ar-op-past-due-consolidated.json',
+        otype='text/html',):
     (
-     # Amounts due get summed correctly
-     get_total_amount_due_tds('$3,300.00'),
-     # Correct notice title <div>
-     get_notice_title_div('FINAL NOTICE OPERATING PERMIT RENEWAL'),
-     # Fee description column is present in invoices table
-     FEE_DESCRIPTION_TH,
-     # Account number right-aligned table cell is present
-     ACCOUNT_NUMBER_RIGHT_ALIGNED_TD,
+        confirm_total_amount_due_is('$3,300.00'),
+        confirm_notice_title_is('PAST DUE NOTICE OPERATING PERMIT RENEWAL'),
+        fee_descr_col_is_present(),
+        account_number_invisi_table_is_twice_present(),
+        collection_action_stmnt_removed(),
+    ),
+
+    Recipe(  # for Demand
+        template='ar_op_demand_consolidated_email',
+        context='ar-op-demand-consolidated.json',
+        otype='text/html',):
+    (
+        confirm_total_amount_due_is('$3,300.00'),
+        confirm_notice_title_is('DEMAND NOTICE'),
+        fee_descr_col_is_present(),
+        account_number_invisi_table_is_twice_present(),
+        ssgr_s18_italicized_present(),
+        collection_mention_removed(),
+        invoice_changed_to_notice(),
+    ),
+
+    Recipe(  # for Final Notice
+        template='ar_op_final_notice_consolidated_email',
+        context='ar-op-final-notice-consolidated.json',
+        otype='text/html',):
+    (
+        confirm_total_amount_due_is('$3,300.00'),
+        confirm_notice_title_is('FINAL NOTICE OPERATING PERMIT RENEWAL'),
+        fee_descr_col_is_present(),
+        account_number_invisi_table_is_twice_present(),
     ),
 
 }
@@ -142,16 +217,18 @@ def validate_document_generation(recipe, generated_doc_path):
     ``recipe`` and downloaded to ``generated_doc_path``.
     """
     try:
-        needles = NEEDLES_BY_RECIPE[recipe]
+        validators = VALIDATORS_BY_RECIPE[recipe]
     except KeyError:
         raise AssertionError(
             f'Unable to validate generated document at'
             f' {generated_doc_path}.'
             f' The recipe {pprint.pformat(recipe)}'
-            f' corresponds to no needles.')
+            f' corresponds to no validators.')
     with open(generated_doc_path) as fh:
-        haystack = fh.read()
-        for needle in needles:
-            assert needle in haystack, (
-                f'Contrary to expectation, substring "{needle}" is not in the'
-                f' file {generated_doc_path}')
+        haystack = fh.read()  # TODO: common interface for bytes and text data.
+        for validator in validators:
+            error = validator(haystack)
+            if error:
+                raise AssertionError(
+                    f'Contrary to expectation, "{error}". Error generated when'
+                    f' validating document at {generated_doc_path}')
